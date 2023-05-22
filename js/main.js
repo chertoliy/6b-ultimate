06e01233f3acb62eef96195327e44099ed8295de
Anatolii Chervontsev <104504367+chertoliy@users.noreply.github.com>
2023-05-22 20:29:19 +0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,77 +1,86 @@
-const button1 = document.querySelector('.dj-set__button1')
-const button2 = document.querySelector('.dj-set__button2')
-const button3 = document.querySelector('.dj-set__button3')
-const button4 = document.querySelector('.dj-set__button4')
-const button5 = document.querySelector('.dj-set__button5')
-const button6 = document.querySelector('.dj-set__button6')
-const button7 = document.querySelector('.dj-set__button7')
-const button8 = document.querySelector('.dj-set__button8')
-const button9 = document.querySelector('.dj-set__button9')
-const button10 = document.querySelector('.dj-set__button10')
-const button11 = document.querySelector('.dj-set__button11')
-const button12 = document.querySelector('.dj-set__button12')
-const button13 = document.querySelector('.dj-set__button13')
-const button14 = document.querySelector('.dj-set__button14')
-const button15 = document.querySelector('.dj-set__button15')
+// const button1 = document.querySelector('.dj-set__button1')
+// const button2 = document.querySelector('.dj-set__button2')
+// const button3 = document.querySelector('.dj-set__button3')
+// const button4 = document.querySelector('.dj-set__button4')
+// const button5 = document.querySelector('.dj-set__button5')
+// const button6 = document.querySelector('.dj-set__button6')
+// const button7 = document.querySelector('.dj-set__button7')
+// const button8 = document.querySelector('.dj-set__button8')
+// const button9 = document.querySelector('.dj-set__button9')
+// const button10 = document.querySelector('.dj-set__button10')
+// const button11 = document.querySelector('.dj-set__button11')
+// const button12 = document.querySelector('.dj-set__button12')
+// const button13 = document.querySelector('.dj-set__button13')
+// const button14 = document.querySelector('.dj-set__button14')
+// const button15 = document.querySelector('.dj-set__button15')
 
-const audio1 = document.querySelector('.audio1');
-const audio2 = document.querySelector('.audio2');
-const audio3 = document.querySelector('.audio3');
-const audio4 = document.querySelector('.audio4');
-const audio5 = document.querySelector('.audio5');
-const audio6 = document.querySelector('.audio6');
-const audio7 = document.querySelector('.audio7');
-const audio8 = document.querySelector('.audio8');
-const audio9 = document.querySelector('.audio9');
-const audio10 = document.querySelector('.audio10');
-const audio11 = document.querySelector('.audio11');
-const audio12 = document.querySelector('.audio12');
-const audio13 = document.querySelector('.audio13');
-const audio14 = document.querySelector('.audio14');
-const audio15 = document.querySelector('.audio15');
+// const audio1 = document.querySelector('.audio1');
+// const audio2 = document.querySelector('.audio2');
+// const audio3 = document.querySelector('.audio3');
+// const audio4 = document.querySelector('.audio4');
+// const audio5 = document.querySelector('.audio5');
+// const audio6 = document.querySelector('.audio6');
+// const audio7 = document.querySelector('.audio7');
+// const audio8 = document.querySelector('.audio8');
+// const audio9 = document.querySelector('.audio9');
+// const audio10 = document.querySelector('.audio10');
+// const audio11 = document.querySelector('.audio11');
+// const audio12 = document.querySelector('.audio12');
+// const audio13 = document.querySelector('.audio13');
+// const audio14 = document.querySelector('.audio14');
+// const audio15 = document.querySelector('.audio15');
 
-button1.onclick = () => {
-   audio1.play();
-}
-button2.onclick = () => {
-   audio2.play();
-}
-button3.onclick = () => {
-   audio3.play();
-}
-button4.onclick = () => {
-   audio4.play();
-}
-button5.onclick = () => {
-   audio5.play();
-}
-button6.onclick = () => {
-   audio6.play();
-}
-button7.onclick = () => {
-   audio7.play();
-}
-button8.onclick = () => {
-   audio8.play();
-}
-button9.onclick = () => {
-   audio9.play();
-}
-button10.onclick = () => {
-   audio10.play();
-}
-button11.onclick = () => {
-   audio11.play();
-}
-button12.onclick = () => {
-   audio12.play();
-}
-button13.onclick = () => {
-   audio13.play();
-}
-button14.onclick = () => {
-   audio14.play();
-}
-button15.onclick = () => {
-   audio15.play();
+// button1.onclick = () => {
+//    audio1.play();
+// }
+// button2.onclick = () => {
+//    audio2.play();
+// }
+// button3.onclick = () => {
+//    audio3.play();
+// }
+// button4.onclick = () => {
+//    audio4.play();
+// }
+// button5.onclick = () => {
+//    audio5.play();
+// }
+// button6.onclick = () => {
+//    audio6.play();
+// }
+// button7.onclick = () => {
+//    audio7.play();
+// }
+// button8.onclick = () => {
+//    audio8.play();
+// }
+// button9.onclick = () => {
+//    audio9.play();
+// }
+// button10.onclick = () => {
+//    audio10.play();
+// }
+// button11.onclick = () => {
+//    audio11.play();
+// }
+// button12.onclick = () => {
+//    audio12.play();
+// }
+// button13.onclick = () => {
+//    audio13.play();
+// }
+// button14.onclick = () => {
+//    audio14.play();
+// }
+// button15.onclick = () => {
+//    audio15.play();
+// }
+
+const button = document.getElementsByClassName('dj-set__button');
+const audio = document.getElementsByClassName('audio');
+
+for (let i = 0; i <= button.length; i++) {
+   button[i].onclick = () => {
+      audio[i].play();
+   }
 }
